38996d4facf26eca5d68a16ddc843254a8a49737
pizzanoodles <jensjovellano@gmail.com>
2021-09-29 10:28:47 +0800

    added multiplication and division feature

--- calculator.py
+++ calculator.py
@@ -2,5 +2,9 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 sum = num1 + num2
 diff = num1 - num2
+prod = num1 * num2
+quot = int(num1 / num2)
 print("The sum of the two numbers is: ", sum)
 print("The difference of the two numbers is: ", diff)
+print("The product of two numbers is: ", prod)
+print("The quotient of two numbers is: ", quot)
