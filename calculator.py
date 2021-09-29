c00525be464167565182376baa625682ffa792a2
pizzanoodles <jensjovellano@gmail.com>
2021-09-29 10:49:22 +0800

    added modulo

--- calculator.py
+++ calculator.py
@@ -4,7 +4,9 @@ sum = num1 + num2
 diff = num1 - num2
 prod = num1 * num2
 quot = int(num1 / num2)
+mod = int(num1%num2)
 print("The sum of the two numbers is: ", sum)
 print("The difference of the two numbers is: ", diff)
 print("The product of two numbers is: ", prod)
 print("The quotient of two numbers is: ", quot)
+print("The modulo of two numbers is: ", mod)
