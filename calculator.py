bf425978d19b62b6a551e065e1c1d8ec0d6f4982
pizzanoodles <jensjovellano@gmail.com>
2021-09-29 11:12:13 +0800

    Named calculator, deleted file2.txt

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+print("Calculator by Jens Jovellano")
+print("*-----------------------------*")
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 sum = num1 + num2
