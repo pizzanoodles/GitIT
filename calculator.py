375c1c6dd08d28baf504720e08e711e153b9010f
pizzanoodles <jensjovellano@gmail.com>
2021-09-29 10:09:49 +0800

    Added subtraction feature.

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 sum = num1 + num2
+diff = num1 - num2
 print("The sum of the two numbers is: ", sum)
+print("The difference of the two numbers is: ", diff)
